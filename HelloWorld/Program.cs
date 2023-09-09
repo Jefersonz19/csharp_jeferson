59e3a281a52d3a1f4543afc08c86531106b849c7
Jefersonz19 <jefersonz1997@gmail.com>
2023-09-08 22:29:33 -0500

    Object Oriented Programming

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace HelloWorld {
+﻿namespace HelloWorld
+{
 
-    class Program {
-        static void Main(){
-            const string Name="Jhon";
-            Console.WriteLine($"Hello World! {Name}");
-            Console.WriteLine();
+    class Program
+    {
+        static void Main()
+        {
+            DateOnly dateConverted = new DateOnly();
+            string NameInput;
+            string birthdayInput;
+            Console.WriteLine("Hello Welcome to Age Calculator!");
+            Console.WriteLine("Write your name here: ");
+            NameInput = Console.ReadLine();
+            Console.WriteLine($"Nice to meet you {NameInput} ");
+            Console.WriteLine("Write your birthday (dd/mm/yy): ");
+            birthdayInput=Console.ReadLine();
+            bool isDateValid=DateOnly.TryParse(birthdayInput,out dateConverted);
+            if (isDateValid==false)
+            Console.WriteLine("The birthday is invalid, Erronous data input: ");
+
+            var person = new Person{
+                Name = NameInput,
+                Birthday=dateConverted,
+                Age=DateTime.Now.Year - dateConverted.Year
+            };
+
+            Console.WriteLine($"Your name {person.Name} ");
+            Console.WriteLine($"Your birthday: {person.Birthday}");
+            Console.WriteLine($"Your are: {person.Age} years old");
+
+            Console.ReadLine();
+
         }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public DateOnly Birthday { get; set; }
     }
 }
